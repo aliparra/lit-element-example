909827d47995292839daf0369dcba0b5aefbb277
aliparra <alicia@redparra.com>
2021-07-13 12:13:40 +0200

    Add some style

--- src/my-counter.js
+++ src/my-counter.js
@@ -16,10 +16,23 @@ export class MyCounter extends LitElement {
 
     render() {
         return html`
-        <div>
+        <style>
+            div{
+                margin: 10px 0px;
+                padding: 10px;
+            }
+
+            .main__title{
+                background-color: pink;
+                width: 200px;
+                border: 2px solid purple
+            }
+        </style>
+        <div class="main__title">
             You have clicked ${this.counter} times
         </div>
         <div>
+            <!-- Los eventos se definen con una @ -->
             <button @click="${this.increment}">+1</button>
             <button @click="${this.decrement}">-1</button>
         </div>
